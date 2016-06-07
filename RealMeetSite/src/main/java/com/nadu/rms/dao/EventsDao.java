fcc96ce02ca92ef4898b33f81c974ea7ee26cdcb
ccg0611 <ccg0611@naver.com>
2016-06-07 09:55:10 +0900

    Events, Review에 해당하는 Dao 및 Mapper 수정, Insert부분만 빼고 새로운 형식으로 수정완료.

--- RealMeetSite/src/main/java/com/nadu/rms/dao/EventsDao.java
+++ RealMeetSite/src/main/java/com/nadu/rms/dao/EventsDao.java
@@ -13,6 +13,8 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.nadu.rms.config.MyBatisUtil;
+import com.nadu.rms.mapper.annotation.EventsMapper;
 import com.nadu.rms.vo.Event_Eventlist;
 import com.nadu.rms.vo.Event_User;
 import com.nadu.rms.vo.Events;
@@ -21,80 +23,59 @@ public class EventsDao {
 
 	private static final Logger log = LoggerFactory.getLogger(EventsDao.class);
 	
-	private SqlSessionFactory getSqlSessionFactory(){
+
+	public List<Event_Eventlist> selectEvents(){
 		
-		String resource="com/nadu/rms/config/mybatis-config.xml";
-		InputStream inputStream;
-
-		try{
-			inputStream = Resources.getResourceAsStream(resource);
-		}catch(IOException e){
-			throw new IllegalArgumentException(e);
-		}
-		return new SqlSessionFactoryBuilder().build(inputStream);
-
-	}
-
-	public List<Events> selectEvents(){
-		SqlSession sqlSession = getSqlSessionFactory().openSession();
-
-		try{
-			String statement = "com.nadu.rms.mapper.EventsMapper.selectEvents";
-			return sqlSession.selectList(statement);
-
-		}finally {
-			sqlSession.close();
-		}
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		EventsMapper mapper = session.getMapper(EventsMapper.class);
+		List<Event_Eventlist> events = mapper.selectEvents();
+		session.close();
+		return events;
 	}
 	
 	
 	public int insertEvents(Event_Eventlist e){
-		SqlSession sqlSession = getSqlSessionFactory().openSession();
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
 		try{
 			String statement = "com.nadu.rms.mapper.EventsMapper.insertEvents";
-			return sqlSession.insert(statement, e);
+			return session.insert(statement, e);
 		}finally{
-			sqlSession.commit(); //commit을 안하면 데이터 입력이 안됩니다.
-			sqlSession.close();
+			session.commit(); //commit을 안하면 데이터 입력이 안됩니다.
+			session.close();
 		}
 	}
 	
 	//이벤트와 유저 정보 획득Map<Object, Object>
 	public List<Event_User> selectEventsNUser(int startNum, int endNum){
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		EventsMapper mapper = session.getMapper(EventsMapper.class);
+		
 		Map<String,Integer> paramMap = new HashMap<String,Integer>();
 		paramMap.put("startNum", startNum);
 		paramMap.put("endNum", endNum);
-		SqlSession sqlSession = getSqlSessionFactory().openSession();
-		try{
-			String statement = "com.nadu.rms.mapper.EventsMapper.selectEventsNUser";
-			return sqlSession.selectList(statement, paramMap);
-		}finally{
-			sqlSession.close();
-		}
+		
+		List<Event_User> events = mapper.selectEventsNUser(paramMap);
+		session.close();
+		
+		return events;
 	}
 	
 	// Events와 EventList 테이블 조인 결과 획득 
 	public List<Event_Eventlist> selectEventsDetailByESIDX(String esidx){
-		SqlSession sqlSession = getSqlSessionFactory().openSession();
-		try{
-			String statement = "com.nadu.rms.mapper.EventsMapper.selectEventsDetailByESIDX";
-			//System.out.println(statement);
-			return sqlSession.selectList(statement, esidx);
-
-		}finally {
-			sqlSession.close();
-		}
+		
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		EventsMapper mapper = session.getMapper(EventsMapper.class);
+		List<Event_Eventlist> events = mapper.selectEventsDetailByESIDX(esidx);
+		session.close();
+		return events;
 	}
 	
 	public int selectCntEvents(){
-		
-		SqlSession sqlSession = getSqlSessionFactory().openSession();
-		try{
-			String statement = "com.nadu.rms.mapper.EventsMapper.selectCntEvents";
-			return sqlSession.selectOne(statement);
-		}finally{
-			sqlSession.close();
-		}
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		EventsMapper mapper = session.getMapper(EventsMapper.class);
+		int cnt = mapper.selectCntEvents();
+		session.close();
+		return cnt;
 	}
 	/*
 	public List<String> getCategories(){
--- RealMeetSite/src/main/java/com/nadu/rms/mapper/annotation/EventsMapper.java
+++ RealMeetSite/src/main/java/com/nadu/rms/mapper/annotation/EventsMapper.java
@@ -3,14 +3,28 @@ package com.nadu.rms.mapper.annotation;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
 import com.nadu.rms.vo.Event_Eventlist;
 import com.nadu.rms.vo.Event_User;
 
 public interface EventsMapper {
+	@Select("SELECT * FROM EVENTS")
 	public List<Event_Eventlist> selectEvents();
-	public List<Event_Eventlist> selectEventsDetailByESIDX(String esidx);
+	
+	@Select("SELECT * FROM VIEW_JOIN_ES_EL WHERE ESIDX = #{esidx, jdbcType=VARCHAR}")
+	public List<Event_Eventlist> selectEventsDetailByESIDX(@Param("esidx")String esidx);
+	
+	@Select("SELECT * FROM (SELECT ROWNUM RNUM, RT.* FROM (SELECT * FROM VIEW_JOIN_ES_USERS) RT ) WHERE RNUM BETWEEN #{startNum, jdbcType=VARCHAR} AND #{endNum, jdbcType=VARCHAR} ORDER BY ESIDX DESC")
 	public List<Event_User> selectEventsNUser(Map<String, Integer> paramMap);
+	
 	public int insertEvents(Event_Eventlist e);
-	public int getCntFromEvents();
+	
+	@Select("SELECT COUNT(ESIDX) FROM EVENTS")
+	public int selectCntEvents();
+	
+	@Select("SELECT CNAME FROM CATEGORIES")
 	public List<String> getCategories();
 }
