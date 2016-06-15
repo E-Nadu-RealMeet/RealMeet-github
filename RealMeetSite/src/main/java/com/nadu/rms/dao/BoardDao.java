240252b540204a6a59b35e75e0622fb09ccf77e5
ccg <ccg0611@naver.com>
2016-06-15 14:06:29 +0900

    labtop -> desktop

--- RealMeetSite/src/main/java/com/nadu/rms/dao/BoardDao.java
+++ RealMeetSite/src/main/java/com/nadu/rms/dao/BoardDao.java
@@ -1,36 +1,15 @@
 package com.nadu.rms.dao;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 
 import com.nadu.rms.config.MyBatisUtil;
 import com.nadu.rms.mapper.annotation.BoardMapper;
-import com.nadu.rms.vo.Events;
 import com.nadu.rms.vo.Board;
 
 public class BoardDao {
-	
-	private SqlSessionFactory getSqlSessionFactory(){
-
-		String resource="com/nadu/rms/config/mybatis-config.xml";
-		InputStream inputStream;
-
-		try{
-			inputStream = Resources.getResourceAsStream(resource);
-		}catch(IOException e){
-			throw new IllegalArgumentException(e);
-		}
-		return new SqlSessionFactoryBuilder().build(inputStream);
-
-	}
 
 	public List<Board> selectBoards(Map<String, Object> paramMap){
 		
@@ -45,55 +24,46 @@ public class BoardDao {
 
 	
 	
-	public Board selectFreeDetail(int nidx){
-		SqlSession sqlSession = getSqlSessionFactory().openSession();
-		try{
-			String statement = "com.nadu.rms.mapper.annotation.BoardMapper.selectBoardDetail";
-			System.out.println(statement);
-			return sqlSession.selectOne(statement, nidx);
-
-		}finally {
-			sqlSession.close();
-		}
+	public Board selectFreeDetail(int bidx){
+		
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		BoardMapper mapper = session.getMapper(BoardMapper.class);
+		Board board = mapper.selectBoardDetail(bidx); 
+		session.close();
+		return board;
 		
 	}
 	
 	public int insertBoard(Board fb){
-		SqlSession sqlSesstion = getSqlSessionFactory().openSession();
-		try {
-			String statement = "com.nadu.rms.mapper.annotation.BoardMapper.insertBoard";
-			return sqlSesstion.insert(statement, fb);
-		} finally {
-			sqlSesstion.commit();
-			sqlSesstion.close();
-		}
+		
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		BoardMapper mapper = session.getMapper(BoardMapper.class);
+		int ret = mapper.insertBoard(fb); 
+		session.commit();
+		session.close();
+		return ret;
 		
 	}
 	
 	public int updateBoard(Board fb){
-		SqlSession sqlSession = getSqlSessionFactory().openSession();
-		try {
-			String statement = "com.nadu.rms.mapper.annotation.BoardMapper.updateBoard";
-			return sqlSession.update(statement, fb);
-		} finally {
-			sqlSession.commit();
-			sqlSession.close();
-		}
+		
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		BoardMapper mapper = session.getMapper(BoardMapper.class);
+		int ret = mapper.updateBoard(fb); 
+		session.commit();
+		session.close();
+		return ret;
+		
 	}
 
-	public void delBoard(String nidx) {
-		SqlSession sqlSession = getSqlSessionFactory().openSession();
-		try{
-			String statement = "com.nadu.rms.mapper.annotation.BoardMapper.delBoard";
-			
-			
-			sqlSession.delete(statement, nidx);
-
-		}finally {
-			sqlSession.commit();
-			sqlSession.close();
-		}
+	public void delBoard(String bidx) {
 		
+		SqlSession session = MyBatisUtil.getSqlSessionFactory().openSession();
+		BoardMapper mapper = session.getMapper(BoardMapper.class);
+		int ret = mapper.delBoard(bidx); 
+		session.commit();
+		session.close();
+				
 	}
 
 	public int getCountByType(String type) {
--- RealMeetSite/src/main/java/com/nadu/rms/mapper/annotation/BoardMapper.java
+++ RealMeetSite/src/main/java/com/nadu/rms/mapper/annotation/BoardMapper.java
@@ -3,8 +3,11 @@ package com.nadu.rms.mapper.annotation;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
 
 import com.nadu.rms.vo.Board;
 
@@ -15,5 +18,17 @@ public interface BoardMapper {
 
 	@Select("SELECT COUNT(BIDX) FROM BOARDS WHERE TYPE = #{type, jdbcType=VARCHAR}")
 	int getCountByType(@Param("type")String type);
+	
+	@Select("SELECT * FROM BOARDS WHERE BIDX = #{bidx, jdbcType=VARCHAR}")
+	Board selectBoardDetail(@Param("bidx")int bidx);
+
+	@Insert("INSERT INTO BOARDS(BIDX, WRITER, TITLE, CONTENT, REGDATE) VALUES(TO_CHAR(BOARDS_SEQ.NEXTVAL), #{writer, jdbcType=NVARCHAR}, #{title, jdbcType=VARCHAR}, #{content, jdbcType=VARCHAR}, SYSDATE)")
+	int insertBoard(Board fb);
+	
+	@Update("UPDATE BOARDS SET TITLE=#{title, jdbcType=VARCHAR}, CONTENT=#{content, jdbcType=VARCHAR}, REGDATE=SYSDATE WHERE BIDX = #{bidx, jdbcType=VARCHAR}")
+	int updateBoard(Board fb);
+	
+	@Delete("DELETE FROM BOARDS WHERE BIDX = #{bidx, jdbcType=VARCHAR}")
+	int delBoard(@Param("bidx")String bidx);
 	
 }
