578ca2e6ccd26076dbdf6e701a7cf739db10cfd1
ccgH <ccg0611@naver.com>
2016-06-18 18:45:06 +0900

    참여기능 수정완료 / 로그인 x자 버튼 클릭가능한 모션으로 변경

--- RealMeetSite/src/main/java/com/nadu/rms/service/EventApplyService.java
+++ RealMeetSite/src/main/java/com/nadu/rms/service/EventApplyService.java
@@ -16,14 +16,30 @@ public class EventApplyService {
 
 	public String applyEvent(String mid, String elidx){
 		
+		String returnString = "" ;
 		Guestlist Vgl = new Guestlist();
 		Vgl.setElidx(elidx);
 		Vgl.setGuest(mid);
 
 		guestlistDao.callApplyProcedure(Vgl);
 		
+		System.out.println("result : "+Vgl.getResult());
 		
-		return Vgl.getResult();
+		
+		if(Vgl.getResult().equals("0")){
+			/* 성공 */
+			returnString = "성공하였습니다.";
+		}
+		else if(Vgl.getResult().equals("1")){
+			/* 자리가 없음 */
+			returnString = "빈자리가 없습니다.";
+		}
+		else if(Vgl.getResult().equals("2")){
+			/* 이미 참여한 경우*/
+			returnString = "이미 참석 하셨습니다.";
+		}
+		
+		return returnString;
 		
 	}
 	public String cancleEvent(String mid, String elidx){
